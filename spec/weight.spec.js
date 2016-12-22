42031276254655bcc55dc64d8a1acb5bcb1ccba9
claireparker <claire.parker87@gmail.com>
2016-12-22 13:54:55 +0000

    Simple tests

--- spec/weight.spec.js
+++ spec/weight.spec.js
@@ -1,5 +1,13 @@
 const weightConverter = require('../weightConverter');
 
-test('blah', () => {
-    expect(weightConverter.add(1, 2)).toBe(3);
+test('kg to stone', () => {
+    expect(weightConverter.kgToStone(1)).toBe(0.16);
 });
+
+test('stone to kg', () => {
+    expect(weightConverter.stoneToKg(1)).toBe(6.35);
+});
+
+test('stone to stone and lbs', ( ) => {
+    
+})
--- weightConverter.js
+++ weightConverter.js
@@ -1,5 +1,16 @@
 module.exports =  {
-    add: function(a, b) {
-        return a + b;
+    FACTOR: 6.35,
+
+    kgToStone: function(kg) {
+        // round to 2 d.p.
+        return Math.round((kg / this.FACTOR) * 100) / 100;
+    },
+
+    stoneToKg: function(stone) {
+        return stone * this.FACTOR;
+    },
+
+    stoneToStoneLbs: function(stone) {
+        return 42;
     }
 }
