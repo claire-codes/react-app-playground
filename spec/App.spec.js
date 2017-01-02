c8d201b927e3fb70e7ae1f1cd85aa814b35384b2
Claire Parker <claire.parker87@gmail.com>
2017-01-02 23:18:14 +0000

    :feelsgood: Testing state changes

--- spec/App.spec.js
+++ spec/App.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow} from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from '../src/App';
 
 it('renders without crashing', () => {
@@ -18,5 +18,26 @@ describe('Enzyme tests', () => {
     it('has exactly three input fields', () => {
         const app = shallow(<App/>);
         expect(app.find('input[type="number"]').length).toEqual(3);
+    });
+});
+
+describe('Behaviour tests', () => {
+    let app;
+
+    beforeEach(() => {
+        app = mount(<App />);
+    });
+
+    it('renders header text', () => {
+        expect(app.find('.App-header').first()).toBeDefined();
+    });
+
+    it('calls handleLbsChange when value changes', () => {
+         const lbsInput = app.find('.z-lbs-input').first();
+         expect(app.state('kg')).toEqual(0);
+         expect(app.state('lbs')).toEqual(0);
+         lbsInput.simulate('change', { target: { value: '2' } });
+         expect(app.state('kg')).toEqual(0.89);
+         expect(app.state('lbs')).toEqual("2");
     });
 });
